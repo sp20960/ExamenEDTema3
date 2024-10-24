fcad2bf3d7cb8ba3b5b7a4fe3bcea598e064835f
Sam <sampf2006@gmail.com>
2024-10-24 17:06:07 +0200

    Primer commit

---
+++ FuncionSuma.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+
+public class FuncionSuma {
+
+	
+	
+	private static int primerSumando;
+	
+	private static int segundoSumando; 
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("Introduce el primer sumando:");
+		primerSumando = in.nextInt();
+		in.nextLine();
+		System.out.println("Introduce el segundo sumando:");
+		segundoSumando = in.nextInt();
+		in.nextLine();
+		System.out.println("La suma es: " + suma(primerSumando, segundoSumando));
+		
+		in.close();
+
+	}
+	
+	
+	
+	private static int suma (int sumando1, int sumando2) {
+		return sumando1-sumando2;
+	}
+
+}
